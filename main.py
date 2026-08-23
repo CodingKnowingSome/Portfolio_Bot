3cde77350a125e93235799d9d5abcb107564c98e
CodingKnowingSome <zsb.szekely@gmail.com>
2026-08-24 01:03:49 +0200

    Took out pytest on startup.

--- main.py
+++ main.py
@@ -107,14 +107,6 @@ async def on_ready():
 
 
 if __name__ == "__main__":
-    print("---Startup Tests---")
-    test_result = subprocess.run(["pytest", "-v"])
-    if test_result.returncode != 0:
-        print("\nPytests failed, stopping bot startup.")
-        logger.error("Pytests failed, stopping bot startup.")
-        sys.exit(1)
-    print("\nAll pytests passed, continue.")
-    logger.info('All pytests passed.')
     api_thread = threading.Thread(target=run_api, daemon=True)
     api_thread.start()
     print("APIs started.")
